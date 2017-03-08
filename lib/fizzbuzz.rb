8267adde747f0b41894d9bab79a1802083f0c2bf
Naz <nazmalik08@gmail.com>
2017-03-08 11:27:48 +0000

    tests passing, code added

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,15 @@
 def fizzbuzz(number)
-  'fizz'
+  if number == 0
+    number
+  elsif number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  else
+    number
+  end
 end
+
+1.upto(100) { |num| puts fizzbuzz(num) }
